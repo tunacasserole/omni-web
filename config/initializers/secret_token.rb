6a7929ecd7d0a3ec050b539c8cf60ea8f4d66800
tunacasserole <aaron@highergroundsolutions.com>
2013-04-11 10:29:15 -0600

    initial build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniWeb::Application.config.secret_token = '199efc554d74b0db93ca1efbbbb09eb349caf7a6b9c94c67303833d9b30e9e6acb61ccaf6f3c1164549b6b0754ea627cb7ec5ab6534d44285f1e63401032a7f0'
